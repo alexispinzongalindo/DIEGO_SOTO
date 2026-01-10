da4717226f3ab22cfdb7771e52a4236fcaba7301
alexispinzongalindo <alexis_pinzon@yahoo.com>
2026-01-10 01:19:29 -0400

    Fix Render runtime and Werkzeug url_parse

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
-from werkzeug.urls import url_parse
+from urllib.parse import urlparse
 from app import db
 from app.auth import bp
 from app.auth.forms import LoginForm, RegistrationForm, ResetPasswordRequestForm, ResetPasswordForm
@@ -23,7 +23,7 @@ def login():
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         
-        if not next_page or url_parse(next_page).netloc != '':
+        if not next_page or urlparse(next_page).netloc != '':
             next_page = url_for('main.dashboard')
             
         return redirect(next_page)
